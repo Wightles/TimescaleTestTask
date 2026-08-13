6f18d3c768f49d0ac66c72032778a9b0b5b434dc
Wightles <aniksshow09@gmail.com>
2026-08-13 13:38:09 +0700

    Добавлены контроллеры и настроен Swagger

---
+++ TimescaleApi/Controllers/HealthController.cs
@@ -0,0 +1,17 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace TimescaleApi.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class HealthController : ControllerBase
+{
+    [HttpGet]
+    public IActionResult Get()
+    {
+        return Ok(new
+        {
+            status = "ok"
+        });
+    }
+}
